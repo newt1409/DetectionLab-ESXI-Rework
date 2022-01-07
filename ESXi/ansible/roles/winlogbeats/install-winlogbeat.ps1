1927bdc30fb7779793b8a85209552aee4626e873
newt1409 <newt1409@hotmail.com>
2022-01-07 15:24:26 -0600

    --- Auto-Git Commit ---

--- ESXi/ansible/roles/winlogbeats/install-winlogbeat.ps1
+++ ESXi/ansible/roles/winlogbeats/install-winlogbeat.ps1
@@ -93,14 +93,14 @@ winlogbeat.event_logs:
 
 #==================== Elasticsearch template setting ==========================
 
-setup.template.settings:
-  index.number_of_shards: 3
+#setup.template.settings:
+#  index.number_of_shards: 3
   #index.codec: best_compression
   #_source.enabled: false
-setup.ilm.enabled: auto
-setup.ilm.rollover_alias: "winlogbeats"
-setup.ilm.policy_name: "winlogbeaters"
-setup.ilm.pattern: "{now/d}"
+#setup.ilm.enabled: false
+#setup.ilm.rollover_alias: "winlogbeats"
+#setup.ilm.policy_name: "winlogbeaters"
+#setup.ilm.pattern: "{now/d}"
 #================================ General =====================================
 
 # The name of the shipper that publishes the network data. It can be used to group
