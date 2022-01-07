99c58c52ee65947b54a653d074a66feb0e2332b3
newt1409 <newt1409@hotmail.com>
2022-01-07 13:23:59 -0600

    --- Auto-Git Commit ---

--- ESXi/ansible/roles/winlogbeats/install-winlogbeat.ps1
+++ ESXi/ansible/roles/winlogbeats/install-winlogbeat.ps1
@@ -97,7 +97,7 @@ setup.template.settings:
   index.number_of_shards: 3
   #index.codec: best_compression
   #_source.enabled: false
-setup.ilm.enabled: auto
+setup.ilm.enabled: yes
 #setup.ilm.rollover_alias: "winlogbeats"
 setup.ilm.pattern: "{now/d}"
 #================================ General =====================================
